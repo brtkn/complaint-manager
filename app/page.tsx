bb04d3dd7ead8b3db1618278091b4ac2a83ddcf4
brtkn <haha>
2024-02-07 14:59:23 +0300

    build the new issue page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@radix-ui/themes';
+import IssuesPage from './issues/page';
 
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-between p-24'>
       <p>Home PAGE!</p>
-      <Button variant='surface'>New Issue</Button>
+      <IssuesPage />
     </div>
   );
 }
