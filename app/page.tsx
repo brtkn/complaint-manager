10793e013a5cf529fe5ce2ad7c1ac6513d64b3c8
brtkn <haha>
2024-02-10 11:01:23 +0300

    show the issues

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import IssuesPage from './issues/page';
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-between p-24'>
-      <p>Home PAGE!</p>
+    <div>
       <IssuesPage />
     </div>
   );
