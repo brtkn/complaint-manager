1f52b7fe3eb9d0b253b1613e142a181d94fac73e
brtkn <haha>
2024-02-17 14:41:28 +0300

    handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,42 +2,65 @@
 import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      await axios.delete('/api/issues/' + issueId);
+      router.push('/issues/list');
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
 
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color='red'>Delete Complaint</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure want to delete this complaint? This action cannot be
-          undone.
-        </AlertDialog.Description>
-        <Flex mt='4' gap='3'>
-          <AlertDialog.Cancel>
-            <Button variant='soft' color='gray'>
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              color='red'
-              onClick={async () => {
-                await axios.delete('/api/issues/' + issueId);
-                router.push('/issues/list');
-                router.refresh();
-              }}
-            >
-              Delete Complaint
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color='red'>Delete Complaint</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure want to delete this complaint? This action cannot be
+            undone.
+          </AlertDialog.Description>
+          <Flex mt='4' gap='3'>
+            <AlertDialog.Cancel>
+              <Button variant='soft' color='gray'>
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color='red' onClick={deleteIssue}>
+                Delete Complaint
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>ERROR</AlertDialog.Title>
+          <AlertDialog.Description>
+            This complaint could not be deleted.
+          </AlertDialog.Description>
+          <Button
+            color='gray'
+            variant='soft'
+            onClick={() => setError(false)}
+            mt='2'
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
