456730a26e29e91b550c9618ab382ec9f5fd1a15
brtkn <haha>
2024-02-24 11:03:23 +0300

    fetch data with react query

--- app/issues/[id]/AssigneeSelecet.tsx
+++ app/issues/[id]/AssigneeSelecet.tsx
@@ -1,19 +1,21 @@
 'use client';
 import { Select } from '@radix-ui/themes';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { User } from 'next-auth';
-import React, { useEffect, useState } from 'react';
-
+import Skeleton from '@/app/component/Skeleton';
 const AssigneeSelecet = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then((res) => res.data),
+  });
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>('/api/users');
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
 
   return (
     <Select.Root>
@@ -21,7 +23,7 @@ const AssigneeSelecet = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
