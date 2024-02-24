6caa33f18644965ffbee851c49505fa4c22079ac
brtkn <haha>
2024-02-24 10:49:57 +0300

    set up react query

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import './theme.config.css';
 import Navbar from './Navbar';
 import { Theme, Container } from '@radix-ui/themes';
 import AuthProvider from './auth/Provider';
+import QueryClientProvider from './QueryClientProvider';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -22,14 +23,16 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme>
-            <Navbar />
-            <main className='p-5'>
-              <Container>{children}</Container>
-            </main>
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme>
+              <Navbar />
+              <main className='p-5'>
+                <Container>{children}</Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
