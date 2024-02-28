f393cd9ea2e7f61169ff695f069e34fc7d7daac8
brtkn <haha>
2024-02-28 23:03:08 +0300

    implament pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Pagination from './component/Pagination';
 import IssuesPage from './issues/list/page';
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <>
-      <Pagination itemCount={100} pageSize={10} currentPage={2} />
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
     </>
   );
 }
