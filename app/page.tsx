c0aaee679a48228e4a31ca592bbddd0a4a7c6865
brtkn <haha>
2024-02-29 12:31:56 +0300

    build the IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
+import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
 import { IssueQuery } from './issues/list/IssueTable';
 import IssuesPage from './issues/list/page';
+import prisma from '@/prisma/client';
 
 interface Props {
   searchParams: IssueQuery;
 }
 
-export default function Home({ searchParams }: Props) {
+export default async function Home({ searchParams }: Props) {
+  const open = await prisma.issue.count({ where: { status: 'OPEN' } });
+  const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+
   return (
     <>
       <LatestIssues />
+      <IssueSummary open={open} closed={closed} inProgress={inProgress} />
     </>
   );
 }
